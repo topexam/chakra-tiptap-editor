dc2443587f9a0c70b8449cfeb329963e9268e229
An Ng. Quang <an.nguyen@fiisoft.com>
2022-01-19 08:27:17 +0700

    feat: add placeholder extension & placeholderText

--- example/index.tsx
+++ example/index.tsx
@@ -8,10 +8,10 @@ const App = () => {
   return (
     <ChakraProvider>
       <Box m={20}>
-        <WebEditor />
+        <WebEditor placeholderText="This is placeholder" />
       </Box>
       <Box m={20}>
-        <WebEditor hasToolbar={false} />
+        <WebEditor hasToolbar={false} placeholderText="This is placeholder" />
       </Box>
     </ChakraProvider>
   );
--- src/editor/index.tsx
+++ src/editor/index.tsx
@@ -2,6 +2,7 @@ import { Box } from '@chakra-ui/react';
 import { useEditor, EditorContent } from '@tiptap/react';
 import StarterKitExt from '@tiptap/starter-kit';
 import UnderlineExt from '@tiptap/extension-underline';
+import Placeholder from '@tiptap/extension-placeholder';
 
 import { BubbleMenuBox } from './menus';
 
@@ -11,12 +12,23 @@ import { CommandMenuExt, CommandSuggestion } from './extensions';
 
 type Props = {
   hasToolbar?: boolean;
+  placeholderText?: string;
 };
-export const WebEditor = ({ hasToolbar = true }: Props) => {
+export const WebEditor = ({ hasToolbar = true, placeholderText }: Props) => {
   const editor = useEditor({
     extensions: [
       StarterKitExt,
       UnderlineExt.configure(),
+      Placeholder.configure({
+        placeholder: ({ node }) => {
+          console.log(placeholderText);
+          if (node.type.name === 'paragraph') {
+            return placeholderText || `Type '/' for commands`;
+          }
+
+          return placeholderText || 'Write content...';
+        },
+      }),
       CommandMenuExt.configure({
         suggestion: CommandSuggestion,
       }),
